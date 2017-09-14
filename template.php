75ff68bc09b8d1e6e61983e0f5cb1a09158ef0de
Charlie Llewellin <charlie@llewellin.net>
2017-09-14 14:10:06 -0500

    adding sponsor ad

--- template.php
+++ template.php
@@ -55,6 +55,21 @@ function cni_preprocess_page(&$variables) {
       }
     }
   }
+
+  /* Sponsor Ad */
+  $variables['node_ad'] = '';
+  if (isset($variables['node'])) {
+    $node = $variables['node'];
+    $ad = field_get_items('node', $node, 'field_ad_image');
+    if (!empty($ad[0]['uri'])) {
+      $arr = array();
+      $arr['img_src'] = file_create_url($ad[0]['uri']);
+      $url = field_get_items('node', $variables['node'], 'field_ad_url');
+      $arr['img_url'] = $url[0]['safe_value'];
+      $variables['node_ad'] = theme_render_template('sites/all/themes/cni/templates/field--field-ad-image--article.tpl.php', $arr);
+    }
+  }
+
 }
 
 function cni_preprocess_node(&$variables) {
