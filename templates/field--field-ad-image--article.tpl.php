725c80d5d2d49c273110190688264dffbea359a5
Charlie Llewellin <charlie@llewellin.net>
2017-12-14 15:27:34 -0500

    sponsor ad target

--- templates/field--field-ad-image--article.tpl.php
+++ templates/field--field-ad-image--article.tpl.php
@@ -1,4 +1,4 @@
 <div class="field-ad-image">
   <div class="field-ad-image-inner">
-    <a href="<?php print $img_url ?>"><img src="<?php print $img_src; ?>" /></a></div>
+    <a href="<?php print $img_url ?>" target="_blank"><img src="<?php print $img_src; ?>" /></a></div>
 </div>
