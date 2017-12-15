c8a348c009ff285e6322fdac7dd6515960d5187d
Charlie Llewellin <charlie@llewellin.net>
2017-12-15 12:05:38 -0500

    sponsor ads

--- templates/field--field-ad-image--article.tpl.php
+++ templates/field--field-ad-image--article.tpl.php
@@ -1,6 +1,8 @@
-<?php foreach ($items as $arr): ?>
+<div class="field-ad-image-wrapper">
+  <?php foreach ($items as $arr): ?>
 <div class="field-ad-image">
   <div class="field-ad-image-inner">
     <a href="<?php print $arr['img_url'] ?>" target="_blank"><img src="<?php print $arr['img_src']; ?>" /></a></div>
 </div>
 <?php endforeach; ?>
+</div>
