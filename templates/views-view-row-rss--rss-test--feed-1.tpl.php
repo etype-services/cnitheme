30b7ffddca246b4234ac612f18f351d9dd9c881e
Charlie Llewellin <charlie@llewellin.net>
2018-01-08 18:29:50 -0600

    rss feed

--- templates/views-view-row-rss--rss-test--feed-1.tpl.php
+++ templates/views-view-row-rss--rss-test--feed-1.tpl.php
@@ -2,8 +2,9 @@
     $img  = '<img src="' . $node->field_image['und'][0]['uri'] . '" />';
 } else {
     $img = '';
-} ?><item>
+} ?>
+<item>
     <title><?php print $title; ?></title>
-    <link><?php print $link; ?></</link>
+    <link><?php print $link; ?>
     <image><?php print trim($img);?></image>
 </item>
