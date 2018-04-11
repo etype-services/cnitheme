26c639ef4c62a6fe6a2886de1c881fee058a4923
etypeweb@non-texas.c.modern-photon-112115.internal <etypeweb@non-texas.c.modern-photon-112115.internal>
2018-04-11 14:37:36 -0500

    add submodule smugmug

--- templates/views-view-row-rss--facebook--facebook-feed.tpl.php
+++ templates/views-view-row-rss--facebook--facebook-feed.tpl.php
@@ -1,6 +1,4 @@
 <item>
     <title><?php print $title; ?></title>
-    <content:encoded><?php print '<![CDATA[<!doctype html><html lang="en" prefix="op: http://media.facebook.com/op#"><head><meta property="fb:pages" content="225561420502" /><link rel="canonical" href="' ?><?php print $link; ?><?php print '"></head><body>' ?><?php print $description;?><?php print '</body></html>]]>' ?></content:encoded>
-    <?php print $item_elements; ?>
+    <content:encoded><?php print $description; ?></content:encoded>
 </item>
-
