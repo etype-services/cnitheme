debe2361cf0a3eb4ccfe628c5f3c88b0ed17c663
etypeweb@non-texas.c.modern-photon-112115.internal <etypeweb@non-texas.c.modern-photon-112115.internal>
2018-04-18 10:28:44 -0500

    updates

--- templates/views-view-row-rss--facebook--facebook-feed.tpl.php
+++ templates/views-view-row-rss--facebook--facebook-feed.tpl.php
@@ -1,4 +1,4 @@
 <item>
     <title><?php print $title; ?></title>
-    <content:encoded><![CDATA[<?php print '<link rel="canonical" href="'?><?php print $link; ?><?php print '">' ?><?php print htmlspecialchars_decode($description); ?>]]></content:encoded>
+    <content:encoded><![CDATA[<?php print '<!doctype html><html lang="en" prefix="op: http://media.facebook.com/op#"><head><meta property="fb:pages" content="225561420502" /><link rel="canonical" href="'?><?php print $link; ?><?php print '"></head><body>' ?><?php print htmlspecialchars_decode($description); ?><?php print '</body></html>' ?>]]></content:encoded>
 </item>
