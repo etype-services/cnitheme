54dd1a2d33befdb8aff63ec4b7fc45ce66620f8e
etypeweb@non-texas.c.modern-photon-112115.internal <etypeweb@non-texas.c.modern-photon-112115.internal>
2018-04-18 08:58:19 -0500

    update facebook feed

--- templates/views-view-row-rss--facebook--facebook-feed.tpl.php
+++ templates/views-view-row-rss--facebook--facebook-feed.tpl.php
@@ -1,4 +1,4 @@
 <item>
     <title><?php print $title; ?></title>
-    <content:encoded><![CDATA[<?php print htmlspecialchars_decode($description); ?>]]></content:encoded>
+    <content:encoded><![CDATA[<?php print '<link rel="canonical" href="'?><?php print $link; ?><?php print '">' ?><?php print htmlspecialchars_decode($description); ?>]]></content:encoded>
 </item>
