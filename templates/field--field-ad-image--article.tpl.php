3e9775b8fb0944dfc36d0e7aaf99d204438b9b97
Charlie Llewellin <charlie@llewellin.net>
2018-04-25 13:17:06 -0500

    add template

--- templates/field--field-ad-image--article.tpl.php
+++ templates/field--field-ad-image--article.tpl.php
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
-<div class="field_ad_image">
-=======
 <div class="field-ad-image">
->>>>>>> 897fc05ad05feae2cd4c66d87a97367f20d4b4b8
   <div class="field_ad_image_inner">
-    <a href="<?php print $img_url ?>"><img src="<?php print $img_src; ?>" /></a></div>
+    <a href="<?php print $img_url ?>" target="_blank"><img src="<?php print
+        $img_src;
+    ?>" /></a></div>
 </div>
