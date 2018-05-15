81dca3ae9486acd75af2cfe18e8fd13920b401cb
Charlie Llewellin <charlie@llewellin.net>
2018-05-15 15:07:18 -0500

    ad scripts

--- template.php
+++ template.php
@@ -316,10 +316,8 @@ function cni_preprocess_html(&$variables) {
   }
 
   /* extra scripts */
-  $head_script = variable_get('etype_head_script');
-  $variables['head_script'] = $head_script;
-  $body_script = empty(variable_get('etype_body_script'))? theme_get_setting('adscript'): variable_get('etype_body_script');
-  $variables['body_script'] = $body_script;
+  $variables['head_script'] = variable_get('etype_head_script');
+  $variables['body_script'] = empty(variable_get('etype_body_script'))? theme_get_setting('adscript'): variable_get('etype_body_script');
 
   /* Menu break variable -- this segment loads different css files depending
   on the setting, and passes a variable that script.js uses to move menus
