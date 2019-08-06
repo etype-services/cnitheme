46a6293e953af3cfe9608828a8b555422ef94f0c
Charlie Llewellin <charlie@llewellin.net>
2019-08-06 14:24:28 +0100

    ada fixes

--- templates/field--field-sponsor-ad-image--article.tpl.php
+++ templates/field--field-sponsor-ad-image--article.tpl.php
@@ -2,7 +2,7 @@
   <?php foreach ($field_sponsor_ad_image_items as $arr): ?>
 <div class="field-ad-image">
   <div class="field-ad-image-inner">
-    <a href="<?php print $arr['img_url'] ?>" target="_blank"><img src="<?php print $arr['img_src']; ?>" /></a></div>
+    <a href="<?php print $arr['img_url'] ?>" target="_blank"><img src="<?php print $arr['img_src']; ?>" alt="Advertisement" /></a></div>
 </div>
 <?php endforeach; ?>
 </div>
